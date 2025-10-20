7d8b33401de58ffccd6b86bc216cc4a68d5d43ba
aharist <aharist03@gmail.com>
2025-10-20 08:56:58 +0700

    penambahan endpoint logi di auth controller dan route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -22,8 +22,9 @@ export const signup = async (req, res, next) => {
             username,
             email,
             password: hashedPassword,
-            role: 'customer', //Tambahkan role ke user object
-            avatar: 'https://g.codewithnathan.com/default-user.png',
+            role: 'customer',
+            watchlist: [],
+            reviews: [],
             createdAt: new Date().toISOString(),
             updatedAt: new Date().toISOString(),
         };
@@ -36,6 +37,24 @@ export const signup = async (req, res, next) => {
             .cookie('taskly_token', token, { httpOnly: true })
             .status(200)
             .json(rest);
+    } catch (error) {
+        next({ status: 500, error });
+    }
+};
+
+export const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await collection.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return next({ status: 401, message: 'Email atau password salah' });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.AUTH_SECRET);
+        const { username } = user;
+        res
+            .cookie('taskly_token', token, { httpOnly: true })
+            .status(200)
+            .json({ username });
     } catch (error) {
         next({ status: 500, error });
     }
