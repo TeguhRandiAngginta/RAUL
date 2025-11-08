ca75ab895dd6ed42a7f19c17ccc5f5dcdf7b388d
aharist <aharist03@gmail.com>
2025-11-08 19:49:22 +0700

    membuat controller watchlist untuk menambah watchlist dan melihat watchlist serta membuat endpointnya

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -5,7 +5,9 @@ import{
     getUser,
     updateUser,
     deleteUser,
-    getUserProfile
+    getUserProfile,
+    toggleWatchlist,
+    getWatchlist
 } from  '../controllers/user.controller.js';
 import { authenticateToken } from '../configs/middleware.js';
 
@@ -18,5 +20,8 @@ router.get('/', getUsers);
 router.get('/:id', getUser);
 router.patch('/update/:id', authenticateToken, updateUser);
 router.delete('/delete/:id', authenticateToken, deleteUser);
+
+router.post('/watchlist/toggle', authenticateToken, toggleWatchlist);
+router.get('/watchlist/me', authenticateToken, getWatchlist);
 
 export default router;
