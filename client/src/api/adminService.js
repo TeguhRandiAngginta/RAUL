af240f12b597be6b12a42acf47ba517d91ea206c
aharist <aharist03@gmail.com>
2025-12-18 19:58:28 +0700

    Implement admin service API functions for user and review management

---
+++ client/src/api/adminService.js
@@ -0,0 +1,31 @@
+import api from './api';
+
+// Ambil Statistik
+export const fetchDashboardStats = async () => {
+    const res = await api.get('/admin/stats');
+    return res.data;
+};
+
+// Ambil Semua User
+export const fetchAllUsers = async () => {
+    const res = await api.get('/users');
+    return res.data;
+};
+
+// Hapus User
+export const deleteUser = async (id) => {
+    const res = await api.delete(`/users/delete/${id}`);
+    return res.data;
+};
+
+// Ambil Semua Review
+export const fetchAllReviews = async () => {
+    const res = await api.get('/reviews/all');
+    return res.data;
+};
+
+// Hapus Review
+export const deleteReview = async (id) => {
+    const res = await api.delete(`/reviews/${id}`);
+    return res.data;
+};
